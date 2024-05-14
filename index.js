bd65214805cfa0dd94980db7e4cfce75686bbd5f
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-05-14 11:06:41 +0530

    add the methods for cors

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const port = process.env.PORT || 5000
 app.use(cors({
     origin: 'https://frontendpizza.vercel.app',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
 
 }));
