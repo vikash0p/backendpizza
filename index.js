a422eea7017fb642b116f42ab078a02d045874b8
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-05-14 09:47:16 +0530

    cors allow anywhere

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ mongodbConnection();
 const app = express()
 const port = process.env.PORT || 5000
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000'],
     credentials: true,
-    
+
 
 }));
 app.use(bodyParser.urlencoded({ extended: true }))
