fed55b5ba64bfa16badc282912ec53950bec087b
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-12 09:59:21 +0530

    solve the error in cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongodbConnection();
 const app = express()
 const port = process.env.PORT || 5000
 app.use(cors({
-    origin: 'https://frontendpizza.vercel.app',
+    origin: ['https://frontendpizza.vercel.app', 'http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
