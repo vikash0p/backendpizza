731274fd8c25d137d9f270bf04c8d5e89a70932e
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-11-12 14:19:37 +0530

    add the  origin in  localhost

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 5000;
 
 // CORS configuration
 app.use(cors({
-    origin: 'https://frontendpizza.vercel.app', 
+    origin: ['https://frontendpizza.vercel.app', 'http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
